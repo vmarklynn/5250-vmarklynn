38f3b47cb631af8e025ea064a4ec92e6cd89092e
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-01-29 14:42:25 -0800

    Partial Checkin – Added UpdateAsync, need to add ID value to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -59,9 +59,25 @@ namespace Mine.Services
             
         }
 
-        public Task<bool> UpdateAsync(ItemModel item)
+        /// <summary>
+        /// Updates the ItemModel in the database
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateAsync(ItemModel item)
         {
-            throw new NotImplementedException();
+           if(item == null)
+            {
+                return false;
+            }
+
+            var result = await Database.UpdateAsync(item);
+            if(result == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public Task<bool> DeleteAsync(string id)
