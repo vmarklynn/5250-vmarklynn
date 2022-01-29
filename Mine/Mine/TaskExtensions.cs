f3fe1cdf8bd1e697cbaa55c1cd89781aa1b69140
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-01-29 12:31:55 -0800

    Added TaskExtension to shared project, TaskExtensions.cs

---
+++ Mine/Mine/TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    public static class TaskExtensions
+    {
+
+        public static async void SafeFireAndForget(this Task task, bool returnToCallingContext, Action<Exception> onException = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(returnToCallingContext);
+            }
+            catch (Exception ex) when (onException != null)
+            {
+                onException(ex);
+            }
+        }
+    }
+}
