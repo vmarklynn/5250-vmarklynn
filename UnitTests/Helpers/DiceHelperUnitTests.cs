31b47d4055a26f49b8c9b243c8401ebcd2ec10e4
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-02-07 16:34:20 -0800

    Added  RollDice_InValid_Roll_1_Dice_0_Should_Return_Zero in DiceHelperUnitTests.cs

--- UnitTests/Helpers/DiceHelperUnitTests.cs
+++ UnitTests/Helpers/DiceHelperUnitTests.cs
@@ -84,5 +84,21 @@ namespace UnitTests.Helpers
 
 
         }
+
+        [Test]
+        public void RollDice_InValid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1,0);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+
+
+        }
     }
 }
