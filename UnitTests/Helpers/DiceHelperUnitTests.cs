5777ad0e1daa842b29f604177881c7f63ebd3c74
Vincent Marklynn <vincent.marklynn@gmail.com>
2022-02-07 16:39:53 -0800

    Added RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15 in DiceHelperUnitTests.cs

--- UnitTests/Helpers/DiceHelperUnitTests.cs
+++ UnitTests/Helpers/DiceHelperUnitTests.cs
@@ -118,5 +118,23 @@ namespace UnitTests.Helpers
             Assert.AreEqual(5, result);
 
         }
+
+        [Test]
+        public void RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15()
+        {
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 5;
+
+            // Act
+            var result = DiceHelper.RollDice(3, 10);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert
+            Assert.AreEqual(15, result);
+
+        }
     }
 }
